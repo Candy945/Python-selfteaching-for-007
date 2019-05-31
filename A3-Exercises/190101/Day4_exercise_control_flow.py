c0673fa9590ab2960b712088027b04fc5752e20e
Candy945 <525387153@qq.com>
2019-05-31 22:03:58 +0800

    Day4

--- A3-Exercises/190101/Day4_exercise_control_flow.py
+++ A3-Exercises/190101/Day4_exercise_control_flow.py
@@ -1,5 +1,18 @@
-
+# 使用Python的for...in循环,打印九九乘法表
 for i in range(1,10):
     for j in range(1,i+1):
-        print("%d*%d=%2d" % (i,j,i*j),end="")
-    print(" ")
+        print("{}*{}={}".format(i,j,i*j),end=" ")
+    print()
+
+# 用Python的while循环,打印九九乘法表,并用条件判断把偶数行去掉
+i = 1
+while i < 10:
+    if i % 2 == 0:
+        i += 1
+        continue
+    j = 1
+    while j < i + 1:
+        print("{}*{}={}".format(i,j,i*j),end=" ")
+        j += 1
+    print()
+    i += 1
